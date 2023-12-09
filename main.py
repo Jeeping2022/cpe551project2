29e735faf6f75e7e732b99c7b2a12666f0c4b83f
christianOConnell <98114720+christianOConnell@users.noreply.github.com>
2023-12-08 20:25:58 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# Author: Christian O'Connell
+# Date: 12/8/2023
+# Description: Drives GameEngine class to manage the game
+
+from GameEngine import GameEngine
+
+def main():
+    ge = GameEngine()
+    ge.initializeGame()
+    ge.intro()
+    numVegLeft = ge.remainingVeggies()
+    score = 0
+    while(numVegLeft > 0):
+        score = ge.getScore()
+        print(f"{numVegLeft} veggies remaining. Current score: {score}")
+        ge.printField()
+        ge.moveRabbits()
+        ge.moveCaptain()
+        numVegLeft = ge.remainingVeggies()
+    # game ends when no veggies remain
+    ge.gameOver()
+    ge.highScore()
+
+main()
